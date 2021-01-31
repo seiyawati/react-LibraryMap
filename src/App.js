1defcfc0cfd7ed31ee4669ea625ae452b8140644
Seiya Kawamoto <kid960805@gmail.com>
2021-01-31 21:34:08 +0900

    add: navitime sdk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import axios from 'axios'
 import { Button } from '@material-ui/core'
+import axios from 'axios'
+
+const api_key = {}
 
 const App = () => {
-
+  const draeMap = () => {
+    axios(api_key)
+      .then((response) => {console.log(response)})
+      .catch((error) => {console.log(error)})
+  }
   return <Button>click</Button>
 }
 
